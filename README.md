0cb3bd23dcef2bc736e8c86507969c774b567d82
Marco van Poortvliet <Marc0tjevp@gmail.com>
2017-10-05 14:10:58 +0200

    Initial upload + ReadMe

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Blog-Theme
-The theme I use for my blog
+
+This theme is made to work with Anchor CMS. If you want to try it yourself you can copy the contents to the themes folder in your Anchor setup.
+The Theme is based on the standard theme, but I adjusted it slightly.
---
+++ footer.php
@@ -0,0 +1,17 @@
+		<div class="wrap">
+	            <footer id="bottom">
+	                <small>&copy; <?php echo date('Y'); ?> <?php echo site_name(); ?>. All rights reserved.</small>
+
+	                <ul role="navigation">
+	                    <li><a href="<?php echo rss_url(); ?>">RSS</a></li>
+
+	                    <li><a href="<?php echo base_url('admin'); ?>" title="Administer your site!">Admin area</a></li>
+
+	                    <li><a href="<?php echo base_url(); ?>" title="Return to my website.">Home</a></li>
+	                </ul>
+	            </footer>
+
+	        </div>
+        </div>
+    </body>
+</html>
